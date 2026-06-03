6b6a4db9f5e5784a4bdbdbf71e3810cb5631257b
Rupesh kumar <rupesh@innovixtechnology.com>
2026-06-03 09:57:10 +0530

    push all backend code in git

--- config/db.js
+++ config/db.js
@@ -1,30 +1,12 @@
-// const mysql = require("mysql2/promise");
-
-// // const pool = mysql.createPool({
-// //   host: "localhost",
-// //   user: "root",
-// //   password: "",
-// //   database: "myapp",
-// // });
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "u627338956_innovixtech",
-//   password: "a4m?R:#joHZ/",
-//   database: "u627338956_innovitecno",
-// });
-
-// module.exports = pool;
-
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
